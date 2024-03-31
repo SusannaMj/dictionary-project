d048d10d90fc590151c65595bff020d8bc1da1c3
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-03-31 19:44:38 +1000

    Initial commit
    - integrated React
    - added Bootstrap
    - added logo

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./dog-and-dictionary.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <a href="#" className="btn btn-primary">
+        {" "}
+        Hello
+      </a>
+    </div>
+  );
+}
+
+export default App;
