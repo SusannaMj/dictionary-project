80944f12a72f02098b41c61d403c357da751dfdb
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-04-06 00:04:01 +1000

    added dictionary component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from "./dog-and-dictionary.jpg";
 import "./App.css";
+import Dictionary from "./dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"> </header>
-      <img src={logo} className="App-logo" alt="logo" />
+      <div className="container">
+        <header className="App-header"> </header>
+        <main>
+          <img src={logo} className="App-logo" alt="logo" />
+          <Dictionary />
+        </main>
+        <footer>This project has been coded by Susanna</footer>
+      </div>
     </div>
   );
 }
