68a1ed5465597c92b9304942b136828f9edffb68
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-04-06 00:06:47 +1000

    adjusted states

--- src/dictionary.js
+++ src/dictionary.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./dictionary.css";
 
 export default function Dictionary() {
-  let [input, setInput] = useState();
+  let [userInput, setInput] = useState("");
   function handleChange(event) {
     event.preventDefault();
     setInput = event.target.value;
