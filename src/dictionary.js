d75939a6f994cd3f75e46fc0e0bf95296f2b819e
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-04-06 00:03:27 +1000

    added basic search function

---
+++ src/dictionary.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./dictionary.css";
+
+export default function Dictionary() {
+  let [input, setInput] = useState();
+  function handleChange(event) {
+    event.preventDefault();
+    setInput = event.target.value;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(setInput);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="search" onChange={handleChange} />
+      </form>
+    </div>
+  );
+}
